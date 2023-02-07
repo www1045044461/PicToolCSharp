3ecbdefcfc2323d6e965d7004e53597d7a710227
Sun ZhiMin <www1045044461@163.com>
2023-02-07 18:18:51 +0800

    111

--- CycleMove/Program.cs
+++ CycleMove/Program.cs
@@ -1,33 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
 namespace CycleMove
 {
+    public class ICMPNameAdd : IComparer<FileInfo>
+    {
+        public static int GetId(string name)
+        {
+            bool is_format1 = name.Contains('_');
+
+            int t1 = -1;
+            int t2 = -1;
+            string _name = null;
+            int id = -1;
+            if (is_format1)
+            {
+                t1 = name.LastIndexOf('_');
+                t2 = name.LastIndexOf('.');
+                _name = name.Substring(t1 + 1, t2 - t1 - 1);
+                id = Convert.ToInt32(_name);
+            }
+            else
+            {
+                id = Convert.ToInt32(name);
+            }
+            return id;
+
+        }
+        public int Compare([AllowNull] FileInfo x, [AllowNull] FileInfo y)
+        {
+            int t1 = GetId(x.Name);
+            int t2 = GetId(y.Name);
+            return t1 >= t2 ? 1 : -1;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
             string path = Directory.GetCurrentDirectory();
-            int n = Convert.ToInt32(args[0]);   
+            //int incMode = Convert.ToInt32(args[0]);
             Console.WriteLine(path);
 
             string[] nativenames = Directory.GetFiles(path);
-            string outputs;
-
             Console.WriteLine($"总共扫描到文件数:{nativenames.Length}");
+            //int mode = Convert.ToInt32(args[0]);
+            string[] _tempnames = new string[nativenames.Length];
 
-            foreach (var item in nativenames)
+            for (int i = 0; i < nativenames.Length; i++)
             {
-               FileInfo fileInfo = new FileInfo(item);
-               var varss  = fileInfo.FullName.Split("\\");
-               var blck = varss[varss.Length - 1];
-               int index11 = blck.IndexOf('_');
-               string num_str = blck.Substring(0, index11 - 1);
-               int count = Convert.ToInt32(num_str);
-               int count1 = count + n;
-               outputs = $"{blck.Substring(index11+1)}";
-               Console.WriteLine($"从{fileInfo.FullName}===>{outputs}");
-               fileInfo.MoveTo(outputs);
+                int t1 = nativenames[i].LastIndexOf('\\');
+                string name = nativenames[i].Substring(t1 + 1);
+                int t2 = name.IndexOf('_');
+                string name1 = name.Substring(t2 + 1);
+                string ttt = Path.Combine(path,name1);
+                _tempnames[i] = ttt;
+                File.Copy(nativenames[i], ttt);
+                File.Delete(nativenames[i]);
             }
         }
     }
